c213c8341c8490a8d02606a06ee361973529c003
Chris <northodontist@gmail.com>
2019-06-20 18:56:26 -0500

    Did a bunch of work since last commit, but it was all following the instructions in the studio so it shouldn't be wrong...probably.

--- src/main/java/org/launchcode/models/Category.java
+++ src/main/java/org/launchcode/models/Category.java
@@ -1,11 +1,20 @@
 package org.launchcode.models;
 
 import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Size;
 
 @Entity
 public class Category {
 
+    @Id
+    @GeneratedValue
     private int id;
+
+    @NotNull
+    @Size(min=3, max=15)
     private String name;
 
     public int getId() {
@@ -17,6 +26,12 @@ public class Category {
     }
 
     public void setName(String name) {
+        this.name = name;
+    }
+
+    public Category() {}
+
+    public Category(String name) {
         this.name = name;
     }
 }
