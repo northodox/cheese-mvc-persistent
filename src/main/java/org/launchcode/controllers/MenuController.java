0dffa50aa7b76240432e5a44ac66f8762c6400cc
Chris <northodontist@gmail.com>
2019-06-24 18:59:25 -0500

    Added MenuController, Menu, and some menu templates

---
+++ src/main/java/org/launchcode/controllers/MenuController.java
@@ -0,0 +1,63 @@
+package org.launchcode.controllers;
+
+import org.launchcode.models.Category;
+import org.launchcode.models.Menu;
+import org.launchcode.models.data.CheeseDao;
+import org.launchcode.models.data.MenuDao;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.validation.Errors;
+import org.springframework.web.bind.annotation.*;
+
+import javax.validation.Valid;
+
+@Controller
+@RequestMapping(value = "menu")
+public class MenuController {
+
+    @Autowired
+    private CheeseDao cheeseDao;
+
+    @Autowired
+    private MenuDao menuDao;
+
+    @RequestMapping(value = "")
+    public String index(Model model) {
+
+        Iterable<Menu> menus = menuDao.findAll();
+        model.addAttribute("menus");
+        model.addAttribute("title", "Menus");
+
+        return "menu/index";
+    }
+
+    @RequestMapping(value = "add", method = RequestMethod.GET)
+    public String displayAddMenuForm(Model model) {
+        model.addAttribute("title", "Add Menu");
+        model.addAttribute(new Menu());
+        return "menu/add";
+    }
+
+    @RequestMapping(value = "add", method = RequestMethod.POST)
+    public String add(Model model, @ModelAttribute @Valid Menu menu,
+                      Errors errors) {
+
+        if (errors.hasErrors()) {
+            model.addAttribute("title", "Add Menu");
+            return "menu/add";
+        }
+
+        menuDao.save(menu);
+        return "redirect:view/" + menu.getId();
+    }
+
+    @RequestMapping(value = "view/{menuId}", method = RequestMethod.GET)
+    public String viewMenu(Model model, @PathVariable int menuId) {
+
+        Menu menu = menuDao.findOne(menuId);
+        model.addAttribute("title", menu.getName());
+        model.addAttribute("cheeses", menu.getCheeses());
+        model.addAttribute("menuId", menu.getId());
+    }
+}
